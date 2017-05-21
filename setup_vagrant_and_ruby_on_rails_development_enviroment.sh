db2d301a78667272998e866e69890d1c33640eca
Josef Zacek <info@josefzacek.com>
2017-05-21 13:58:45 +0100

    if Vagrantfile doesn't exists create one and show user notice to update it

--- setup_vagrant_and_ruby_on_rails_development_enviroment.sh
+++ setup_vagrant_and_ruby_on_rails_development_enviroment.sh
@@ -15,3 +15,23 @@ if [ -f "$vagrantFilePath" ]
 then
   echo "$vagrantFile already exists!"
   echo "Exiting"
+else
+  echo "------------ INIT VAGRANT ------------"
+  vagrant init ubuntu/trusty64
+  sleep 5
+  echo $'
+  <<<<< -- Please add following to Vagrantfile -- >>>>>
+
+    config.vm.network :forwarded_port, guest: 3000, host: 3000
+
+  <<<<< -- Please update Vagrantfile with following (optional) -- >>>>>
+
+    config.vm.provider "virtualbox" do |vb|
+    # Display the VirtualBox GUI when booting the machine
+    # vb.gui = true
+    
+    # Customize the amount of memory on the VM:
+      vb.memory = "2048"
+    end
+  '
+fi
