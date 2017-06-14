478cc90fc302dc2169d3fb37b68aee14fa76c0d1
Josef Zacek <info@josefzacek.com>
2017-06-14 01:03:28 +0100

    set colors used for output

--- setup_rbenv_ruby_rails_mysql_on_vagrant.sh
+++ setup_rbenv_ruby_rails_mysql_on_vagrant.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 echo "------------ CHECK VAGRANT VERSION ------------"
+# set colors colors
+GREEN='\e[1;32m'
+BLUE='\e[1;36m'
+YELLOW='\e[1;33m'
+RED='\e[1;31m'
+WHITE='\e[1;37m'
+
 vagrant -v
 sleep 2
 
