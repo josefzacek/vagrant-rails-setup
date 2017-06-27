13607938edcae4d95ca25ba6281a686e20ffe4c7
Josef Zacek <info@josefzacek.com>
2017-06-27 23:59:00 +0100

    fix formatting

--- setup_rbenv_ruby_rails_mysql_on_vagrant.sh
+++ setup_rbenv_ruby_rails_mysql_on_vagrant.sh
@@ -42,7 +42,7 @@ printf "
   ${BLUE}config.vm.provider 'virtualbox' do |vb|
     # Display the VirtualBox GUI when booting the machine
     # vb.gui = true
-  
+
     # Customize the amount of memory on the VM:
     vb.memory = '2048'
   end
