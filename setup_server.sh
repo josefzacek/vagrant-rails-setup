54a12e90f1ed69b3e6db33fc3e60229ac7770ca1
Josef Zacek <info@josefzacek.com>
2017-07-10 23:44:37 +0100

    ask user if they would like to install updates

--- setup_server.sh
+++ setup_server.sh
@@ -6,3 +6,24 @@ BLUE='\e[1;36m'
 YELLOW='\e[1;33m'
 RED='\e[1;31m'
 WHITE='\e[1;37m'
+
+# would you like to install updates
+printf "${YELLOW} Would you like to run update command (y/n)? ${WHITE}\n"
+read serverUpdate
+
+if [ $serverUpdate == "y" ]
+  then
+    printf "${BLUE} ------------  Running ${GREEN} cd / ${BLUE} command to navigate to root folder  ------------ ${WHITE}\n"
+    cd /
+    sleep 2
+    printf "${BLUE} ------------  Running ${GREEN} pwd ${BLUE} command to confirm path  ------------ ${WHITE}\n"
+    pwd
+    sleep 5
+    printf "${BLUE} ------------  Running ${GREEN} sudo apt-get update ${BLUE} command to get the latest update  ------------ ${WHITE}\n"
+    sudo apt-get update
+elif [ $serverUpdate == "n" ]
+  then
+    echo "You have answered No, exiting!"
+else
+    echo "invalid input exiting"
+fi
